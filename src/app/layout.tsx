2f9cf436cba8157e284cbe80b2d5a3dc5312c2b1
Rohit <rohitsrivas83@gmail.com>
2025-05-31 22:10:01 +0530

    ad

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,6 +10,9 @@ export default function RootLayout({ children }: { children: ReactNode }) {
     return (
         <html lang="en">
         <head>
+            <script async
+                    src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1723901698088903"
+                    crossOrigin="anonymous"></script>
 
         </head>
         <body>
